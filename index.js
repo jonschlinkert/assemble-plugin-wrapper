8cf8c2ca752099c7234366abd28c5a44d585b896
Brian Woodward <brian.woodward@gmail.com>
2015-09-13 14:34:14 -0400

    implementation and tests

--- index.js
+++ index.js
@@ -1,12 +1,81 @@
 /*!
  * assemble-plugin-wrapper <https://github.com/doowb/assemble-plugin-wrapper>
  *
- * Copyright (c) 2015, Brian Woodward.
+ * Copyright (c) 2015, Brian Woodward, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
-};
+var path = require('path');
+
+function wrapPlugin(pluginType, fn) {
+  if (typeof pluginType === 'function') {
+    fn = pluginType;
+    pluginType = 'any';
+  }
+
+  pluginType = pluginType || 'any';
+
+  return function (obj) {
+    /**
+     * App plugin
+     */
+
+    if ((pluginType === 'app' || pluginType === 'any') && obj.isApp) {
+      extendSelf.call(this, obj, fn);
+    }
+
+    /**
+     * Collection plugin
+     */
+
+    if (pluginType === 'collection' || pluginType === 'any') {
+      if (obj.isApp) {
+        extendCollection.call(this, obj, fn);
+      }
+      if (obj.isCollection) {
+        extendSelf.call(this, obj, fn);
+      }
+    }
+
+    /**
+     * View plugin
+     */
+
+    if (pluginType === 'view' || pluginType === 'any') {
+      if (obj.isApp || obj.isCollection) {
+        extendViews.call(this, obj, fn);
+      }
+      if (obj.isView) {
+        extendSelf.call(this, obj, fn);
+      }
+    }
+  };
+}
+
+function extendCollection(obj, fn) {
+  obj.options.extendViews = function (collection) {
+    fn.call(collection, collection);
+    return collection;
+  };
+}
+
+function extendViews(obj, fn) {
+  var orig = obj.extendView;
+  obj.define('extendView', function () {
+    var view = orig.apply(this, arguments);
+    extendSelf.call(view, view, fn);
+    return view;
+  });
+}
+
+function extendSelf(self, fn) {
+  fn.call(self, self);
+}
+
+/**
+ * Expose `wrapPlugin`
+ */
+
+module.exports = wrapPlugin;
---
+++ test.js
@@ -0,0 +1,97 @@
+require('mocha');
+var assert = require('assert');
+var assemble = require('assemble');
+var wrap = require('./');
+var plugin;
+
+describe('plugin defaults', function () {
+  beforeEach(function () {
+    plugin = function (type) {
+      return wrap(type, function () {
+        this.foo = function (str) {
+          return str + 'foo';
+        };
+      });
+    };
+  })
+
+  it('should work as an app plugin:', function () {
+    var app = assemble()
+      .use(plugin());
+
+    assert(app.foo('bar') === 'barfoo');
+  });
+
+  it('should work on collections as an app plugin:', function () {
+    var app = assemble()
+      .use(plugin('collection'));
+
+    app.create('posts');
+    assert(app.posts.foo('bar') === 'barfoo');
+  });
+
+  it('should work on views as an app plugin:', function () {
+    var app = assemble()
+      .use(plugin('view'));
+
+    app.create('posts');
+    app.post('aaa', {content: '...'});
+    assert(app.views.posts.aaa.foo('bar') === 'barfoo');
+  });
+
+  it('should work on app, collections and views as an app plugin:', function () {
+    var app = assemble()
+      .use(plugin());
+
+    app.create('posts');
+    app.post('aaa', {content: '...'});
+    assert(app.foo('bar') === 'barfoo');
+    assert(app.posts.foo('bar') === 'barfoo');
+    assert(app.views.posts.aaa.foo('bar') === 'barfoo');
+  });
+
+  it('should work as a create plugin:', function () {
+    var app = assemble();
+    app.create('pages')
+      .use(plugin());
+
+    app.pages('aaa', {content: '...'});
+    assert(app.pages.foo('bar') === 'barfoo');
+  });
+
+
+  it('should work as a collection plugin:', function () {
+    var app = assemble();
+    app.create('pages')
+    app.pages.use(plugin());
+
+    app.pages('aaa', {content: '...'});
+    assert(app.pages.foo('bar') === 'barfoo');
+  });
+
+  it('should work on collections and views as a collection plugin:', function () {
+    var app = assemble();
+    app.create('pages')
+      .use(plugin('any'))
+
+    app.pages('aaa', {content: '...'});
+    assert(app.pages.foo('bar') === 'barfoo');
+    assert(app.views.pages.aaa.foo('bar') === 'barfoo');
+  });
+
+  it('should work as a view plugin:', function () {
+    var app = assemble();
+    app.create('pages');
+
+    app.page('aaa', {content: '...'})
+      .use(plugin());
+
+    assert(app.pages.views.aaa.foo('bar') === 'barfoo');
+  });
+
+  // it('should throw an error:', function () {
+  //   (function () {
+  //     plugin();
+  //   }).should.throw('plugin expects valid arguments');
+  // });
+});
